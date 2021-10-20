e026c8376d5face3ea57b540db34734140307577
Gilberto <gilbertosa099@outlook.com>
2021-10-20 16:53:47 -0500

    [FIX] sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -28,12 +28,15 @@ module.exports ={
                 }
             },
             {
-                test: /\.css|sass$/,
-                use:[
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
             }
 
         ]
